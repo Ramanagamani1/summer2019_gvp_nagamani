8bffa7c1d7066582813e71209f6e9960f66d24bd
Ramanagamani1 <ramanagamani1@gmail.com>
2019-06-23 16:57:19 +0530

    renamed

--- Appday/iplapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from iplapp.models import Matches,Deliveries
-
-admin.site.register(Matches)
-
-
-admin.site.register(Deliveries)
--- Appday/iplapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IplappConfig(AppConfig):
-    name = 'iplapp'
