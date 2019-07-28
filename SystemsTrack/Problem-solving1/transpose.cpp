c8f004ed09219e1ef14df5a52741678bb016378d
Nagamani <ramanagamani1@gmail.com>
2019-07-28 20:26:02 +0530

    Update transpose.cpp

--- SystemsTrack/Problem-solving1/transpose.cpp
+++ SystemsTrack/Problem-solving1/transpose.cpp
@@ -1,36 +1,36 @@
 ///*transpose*/
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//#include<string.h>
-//#include<malloc.h>
-//#include<conio.h>
-//void transpose(int **a, int row, int column){
-//	/*int res[10][10];
-//	for (int i = 0; i < row; i++){
-//		for (int j = 0; j < column; j++){
-//			res[i][j] = a[j][i];
-//		}
-//	}*/
-//	for (int i = 0; i < column; i++){
-//		for (int j = 0; j < row; j++)
-//			printf("%d ", a[j][i]);
-//		printf("\n");
-//	}
-//}
-//int main(){
-//	int n, m;
-//	printf("enter no of rows and columns");
-//	scanf("%d %d", &n, &m);
-//	int **a = (int **)malloc(n*sizeof(int));
-//	for (int i = 0; i < n; i++)
-//		a[i] = (int *)malloc(m*sizeof(int));
-//	for (int i = 0; i < n; i++){
-//		for (int j = 0; j < m; j++){
-//			scanf("%d", &a[i][j]);
-//		}
-//	}
-//	transpose(a, n, m);
-//
-//	_getch();
-//	return 0;
-//}
+#define _crt_secure_no_warnings
+#include<stdio.h>
+#include<string.h>
+#include<malloc.h>
+#include<conio.h>
+void transpose(int **a, int row, int column){
+	/*int res[10][10];
+	for (int i = 0; i < row; i++){
+		for (int j = 0; j < column; j++){
+			res[i][j] = a[j][i];
+		}
+	}*/
+	for (int i = 0; i < column; i++){
+		for (int j = 0; j < row; j++)
+			printf("%d ", a[j][i]);
+		printf("\n");
+	}
+}
+int main(){
+	int n, m;
+	printf("enter no of rows and columns");
+	scanf("%d %d", &n, &m);
+	int **a = (int **)malloc(n*sizeof(int));
+	for (int i = 0; i < n; i++)
+		a[i] = (int *)malloc(m*sizeof(int));
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < m; j++){
+			scanf("%d", &a[i][j]);
+		}
+	}
+	transpose(a, n, m);
+
+	_getch();
+	return 0;
+}
