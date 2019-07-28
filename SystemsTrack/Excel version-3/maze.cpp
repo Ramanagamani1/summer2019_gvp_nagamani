1914782bac68d94461cb9d35eaeecc969a9e6dce
Nagamani <ramanagamani1@gmail.com>
2019-07-28 20:31:12 +0530

    Update maze.cpp

--- SystemsTrack/Excel version-3/maze.cpp
+++ SystemsTrack/Excel version-3/maze.cpp
@@ -1,4 +1,4 @@
-/*#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
@@ -64,4 +64,4 @@ int main(){
 		printf("no path");
 	}
 	_getch();
-}*/
+}
